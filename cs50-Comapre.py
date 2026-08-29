b4e8af26550fa27c7a355eac75b80e261401f0d5
Anushka Sharma <anushkasharmatech08@gmail.com>
2026-08-29 22:11:54 +0530

    29th August : and , or

--- cs50-Comapre.py
+++ cs50-Comapre.py
@@ -6,3 +6,31 @@ elif x>y:
     print("y is less than x")
 else:
     print("x and y are equal")
+
+# or
+
+if x<y or x>y:
+    print("x and y are not equal")
+else:
+    print("x and y are equal")
+
+# !
+if x!=y :
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+#and
+score=int(input("Score:"))
+if score>=90 and score<=100:
+    print("Grade: A")
+# another way to write the conditions succinctly:
+elif 80<=score<90:
+    print("Grade: B")
+# another way to write it succinctly :
+elif score>=70:
+    print("Grade: C")
+elif score>=60:
+    print("Grade: D")
+else:
+    print("Grade: F")
