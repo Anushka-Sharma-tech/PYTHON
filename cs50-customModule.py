f25a18991321bbaf74dbf3de79953895a9e9991c
Anushka Sharma <anushkasharmatech08@gmail.com>
2026-09-12 23:00:21 +0530

    12th September-> Created a custom module

---
+++ cs50-customModule.py
@@ -0,0 +1,4 @@
+import sys
+from cs50_saying import hello
+if len(sys.argv)==2:
+    hello(sys.argv[1])
---
+++ cs50_saying.py
@@ -0,0 +1,9 @@
+def main():
+    hello("world")
+    goodbye("world")
+def hello(name):
+    print("Hello,",name)
+def goodbye(name):
+    print(f"Goodbye, {name}")
+if __name__=="__main__":
+    main()
